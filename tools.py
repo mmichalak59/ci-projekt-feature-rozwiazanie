d0bbdf59cd82b3453b749ae86ef7ca350e788a23
maciej.michalak <maciej.michalak@sysadmins.pl>
2026-04-17 09:05:02 +0200

    Dodano funkcje policz_znaki z testem

---
+++ tools.py
@@ -0,0 +1,16 @@
+def na_wielkie(tekst):
+    return tekst.upper()
+
+def na_male(tekst):
+    return tekst.lower()
+
+def policz_slowa(tekst):
+    if not tekst.strip():
+        return 0
+    return len(tekst.split())
+
+def odwroc(tekst):
+    return tekst[::-1]
+
+def policz_znaki(tekst):
+    return len(tekst)
